1aa87f75c5d1564dd2e18639ea3e02f22e61fa54
rezakalfane <reza.kalfane@gmail.com>
2021-11-03 13:17:54 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -33,6 +33,7 @@ export class Identifiable {
 export class CommerceObject extends Identifiable {
     slug: string
     name: string
+    raw: string
 }
 
 export class Product extends CommerceObject {
